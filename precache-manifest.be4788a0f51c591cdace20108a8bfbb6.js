84a5a70eba2152a7afb7161a35956a49a4239844
hcc1232001 <qes4d26@hotmail.com>
2020-10-30 19:43:17 +0800

    Updates

---
+++ precache-manifest.be4788a0f51c591cdace20108a8bfbb6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61aa5fa03a4bffd82be15ef15456f697",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9f70f5192c762081b7e",
+    "url": "./static/css/main.07555382.chunk.css"
+  },
+  {
+    "revision": "8cd9c25f2ce1fc56e598",
+    "url": "./static/js/2.d0fd5c86.chunk.js"
+  },
+  {
+    "revision": "a9f70f5192c762081b7e",
+    "url": "./static/js/main.140f9981.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3f43f4c27bb3a761afef7337d814ab3f",
+    "url": "./static/media/190715_CLP_game(8).3f43f4c2.fbx"
+  },
+  {
+    "revision": "76c68fdb63056744a61938f617069e86",
+    "url": "./static/media/190717_frying pan animationA_flip.76c68fdb.fbx"
+  },
+  {
+    "revision": "0f9bee900d1ee92c52af251e9160649d",
+    "url": "./static/media/190717_frying pan animationA_shake.0f9bee90.fbx"
+  },
+  {
+    "revision": "73bcd93e70dcd7923c0f51a9c453195d",
+    "url": "./static/media/190717_frying pan animationB_flip.73bcd93e.fbx"
+  },
+  {
+    "revision": "391437456c093d55ebc6ac31d1bc016d",
+    "url": "./static/media/190717_frying pan animationB_shake.39143745.fbx"
+  },
+  {
+    "revision": "e5174b4e660fcc7f31191a1cfc07fb98",
+    "url": "./static/media/190717_frying pan animationC_flip.e5174b4e.fbx"
+  },
+  {
+    "revision": "642bef3ecf63cb2feb1efdc11db65f05",
+    "url": "./static/media/190717_frying pan animationC_shake.642bef3e.fbx"
+  },
+  {
+    "revision": "826671b9f167fc302e28e36e74938690",
+    "url": "./static/media/click.826671b9.mp3"
+  },
+  {
+    "revision": "5069922d79b7e5b0297b40716f3aa0d9",
+    "url": "./static/media/meat.5069922d.png"
+  },
+  {
+    "revision": "6081577eae57d82b692803d8989d8ca7",
+    "url": "./static/media/mid_meat.6081577e.png"
+  },
+  {
+    "revision": "0da2595b3a939c90a760c34cb4a1889f",
+    "url": "./static/media/welldone_meat.0da2595b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3104758fe89ea5c17bc29c6d787b47dc.js"
+  "./precache-manifest.be4788a0f51c591cdace20108a8bfbb6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
